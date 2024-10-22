b5f8e8aab077a4be32e4283e859df5df0a027878
PaksF <flaksfjtm@gmail.com>
2024-10-22 16:28:26 +0900

    Initial commit

---
+++ src/main/java/kr/ac/kopo/lego_guestbook/LegoGuestbookApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.lego_guestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoGuestbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegoGuestbookApplication.class, args);
+    }
+
+}
